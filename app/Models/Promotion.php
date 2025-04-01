6b371ce16fe57af4113943391bdea9503577c9fd
steveharianto <steveharianto@gmail.com>
2025-04-01 18:21:37 +0700

    Setup Model & Route

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Promotion extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = ['title', 'description', 'image'];
+    
+    public function getImageUrlAttribute()
+    {
+        return asset('storage/' . $this->image);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PromotionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PromotionController::class, 'index'])->name('home');
+Route::resource('promotions', PromotionController::class);
